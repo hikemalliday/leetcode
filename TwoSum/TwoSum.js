97f5e654effde564d9911d620e9054f657f56879
hikemalliday <hikemalliday@gmail.com>
2022-11-27 15:17:03 -0600

    leetcode practice

---
+++ TwoSum/TwoSum.js
@@ -0,0 +1,20 @@
+//Return the indexis of the two numbers in the array that equal the target
+
+array = [1, 2, 3];
+target = 5;
+
+const twoSum = function(nums, target)
+{
+    for (let i = 0; i < nums.length; i++)
+    {
+        for (let j = 1; j < nums.length; j++)
+        {
+            if (nums[i] + nums[j] == target)
+            {
+                return [i, j];
+            }
+        }
+    }
+};
+
+console.log(twoSum(array, target));
